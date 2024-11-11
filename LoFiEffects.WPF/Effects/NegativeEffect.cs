83cb000abd980d17a1f73803c4bc0c167186aea9
ben_singer <ben@singerinstruments.com>
2024-11-11 15:51:33 +0000

    Resolving duplication issue

--- LoFiEffects.WPF/Effects/NegativeEffect.cs
+++ LoFiEffects.WPF/Effects/NegativeEffect.cs
@@ -1,15 +1,39 @@
-﻿using System.Windows.Media.Effects;
+﻿using System.Windows.Media;
+using System.Windows;
+using System.Windows.Media.Effects;
 
 namespace LoFiEffects.WPF.Effects
 {
     /// <summary>
     /// Represents a negative shader effect.
     /// </summary>
-    public class NegativeEffect : SpecklingEffect
+    public class NegativeEffect : ShaderEffect
     {
         #region StaticFields
 
         private static readonly PixelShader pixelShader = new() { UriSource = UriHelper.FromResource(@"Effects/Shaders/Negative.ps") };
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Get or set the input. This is a dependency property.
+        /// </summary>
+        public Brush Input
+        {
+            get { return (Brush)GetValue(InputProperty); }
+            set { SetValue(InputProperty, value); }
+        }
+
+        #endregion
+
+        #region DependencyProperties
+
+        /// <summary>
+        /// Identifies the NegativeEffect.Input property.
+        /// </summary>
+        public static readonly DependencyProperty InputProperty = RegisterPixelShaderSamplerProperty("Input", typeof(NegativeEffect), 0);
 
         #endregion
 
