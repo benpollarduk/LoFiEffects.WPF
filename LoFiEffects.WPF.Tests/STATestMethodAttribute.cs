c0cb457a10d158d94a13b4fe7f48f72830b52936
benpollarduk <bajpollard@gmail.com>
2026-05-18 11:47:49 +0100

    Updated to .NET 10

--- LoFiEffects.WPF.Tests/STATestMethodAttribute.cs
+++ LoFiEffects.WPF.Tests/STATestMethodAttribute.cs
@@ -1,17 +1,19 @@
-﻿namespace LoFiEffects.WPF.Tests
+﻿using System.Runtime.CompilerServices;
+
+namespace LoFiEffects.WPF.Tests
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 #pragma warning disable S101 // Types should be named in PascalCase
-    public class STATestMethodAttribute : TestMethodAttribute
+    public class STATestMethodAttribute([CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = 0) : TestMethodAttribute(callerFilePath, callerLineNumber)
 #pragma warning restore S101 // Types should be named in PascalCase
     {
-        public override TestResult[] Execute(ITestMethod testMethod)
+        public override async Task<TestResult[]> ExecuteAsync(ITestMethod testMethod)
         {
             var result = Array.Empty<TestResult>();
 
-            var thread = new Thread(() =>
+            var thread = new Thread(async () =>
             {
-                result = base.Execute(testMethod);
+                result = await base.ExecuteAsync(testMethod);
             });
 
             thread.SetApartmentState(ApartmentState.STA);
